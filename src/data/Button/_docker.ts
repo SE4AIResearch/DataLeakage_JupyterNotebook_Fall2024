7e3454fb8558e5587dd0ed1b0793fbf7f585c6a7
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2025-03-03 20:34:20 -0500

    docker changes; hid group id column

--- src/data/Button/_docker.ts
+++ src/data/Button/_docker.ts
@@ -1,33 +1,36 @@
 import * as vscode from 'vscode';
-import Docker, { ImageInfo } from 'dockerode';
+import Docker, { ContainerCreateOptions, ImageInfo } from 'dockerode';
 
 import { DockerTemp, TempDir } from '../../helpers/TempDir';
 
-export async function ensureImageExist(docker: Docker, imageName: string) {
+async function downloadImage(docker: Docker) {
   try {
     const images: ImageInfo[] = await docker.listImages();
 
     const getImage = async () =>
-      images.find((image) => image.RepoTags?.includes(`${imageName}:latest`));
+      images.find((image) => image.RepoTags?.includes(DockerTemp.IMAGE_NAME));
 
     if (!(await getImage())) {
       const imageRes = await new Promise((resolve, reject) => {
-        docker.pull(imageName, (pullError: any, pullStream: any) => {
-          if (pullError) {
-            reject(pullError);
-          }
-          docker.modem.followProgress(
-            pullStream,
-            (progressError: any, output: any) => {
-              if (progressError) {
-                reject(progressError);
-              } else {
-                resolve(output);
-                return;
-              }
-            },
-          );
-        });
+        docker.pull(
+          DockerTemp.IMAGE_NAME,
+          (pullError: any, pullStream: any) => {
+            if (pullError) {
+              reject(pullError);
+            }
+            docker.modem.followProgress(
+              pullStream,
+              (progressError: any, output: any) => {
+                if (progressError) {
+                  reject(progressError);
+                } else {
+                  resolve(output);
+                  return;
+                }
+              },
+            );
+          },
+        );
       });
     }
   } catch (err) {
@@ -36,27 +39,79 @@ export async function ensureImageExist(docker: Docker, imageName: string) {
   }
 }
 
+async function getExistingContainer(docker: Docker, containerName: string) {
+  try {
+    const containers = await docker.listContainers({ all: true });
+
+    const targetContainer = containers.find((container) => {
+      return container.Names.some(
+        (name) => name === `/${containerName}` || name === containerName,
+      );
+    });
+    if (!targetContainer) {
+      console.log(`Container '${containerName}' not found`);
+      return null;
+    }
+
+    const container = docker.getContainer(targetContainer.Id);
+
+    if (targetContainer.State === 'running') {
+      console.log(
+        `Container '${containerName}' is running, forcing it to stop`,
+      );
+
+      await container.stop({ t: 0 }); // t: 0 means no grace period, force kill
+      console.log(`Container '${containerName}' has been stopped`);
+    } else {
+      console.log(`Container '${containerName}' exists but is not running`);
+    }
+
+    return container;
+  } catch (error) {
+    console.error('Error handling container:', error);
+    throw error;
+  }
+}
+
+async function createContainer(docker: Docker, tempDir: TempDir) {
+  try {
+    const config: ContainerCreateOptions = {
+      name: 'DataLeakageAlgo',
+      Image: DockerTemp.IMAGE_NAME,
+      Cmd: [`${DockerTemp.PYTHON_FILE_PATH}`, `-o`],
+      HostConfig: {
+        Binds: [`${tempDir.getAlgoDirPath()}:${DockerTemp.CONTAINER_DIR_PATH}`],
+      },
+      platform: 'linux/amd64',
+    };
+
+    const container =
+      (await getExistingContainer(docker, config.name!)) ??
+      (await docker.createContainer(config));
+
+    const exec = await container.attach({
+      stream: true,
+      stdout: true,
+      stderr: true,
+    });
+
+    exec.pipe(process.stdout);
+
+    return container;
+  } catch (error) {
+    console.error('Error creating container:', error);
+    throw error;
+  }
+}
+
 export async function runDocker(tempDir: TempDir) {
   const docker = new Docker();
-  await ensureImageExist(docker, DockerTemp.IMAGE_NAME);
+  await downloadImage(docker);
+  const container = await createContainer(docker, tempDir);
 
   const timeout = new Promise((res) =>
-    setTimeout(() => res('Timed out'), 20000),
+    setTimeout(() => res('Timed out'), 1000 * 60 * 60),
   );
 
-  const result = await Promise.race([
-    docker.run(
-      DockerTemp.IMAGE_NAME,
-      [`${DockerTemp.PYTHON_FILE_PATH}`, `-o`],
-      process.stdout,
-      {
-        HostConfig: {
-          Binds: [
-            `${tempDir.getAlgoDirPath()}:${DockerTemp.CONTAINER_DIR_PATH}`,
-          ],
-        },
-      },
-    ),
-    timeout,
-  ]);
+  const result = await Promise.race([container.start(), timeout]);
 }
--- src/helpers/TempDir.ts
+++ src/helpers/TempDir.ts
@@ -73,7 +73,7 @@ export class TempDir {
 // FIXME: Temporary location until we replace docker
 
 export class DockerTemp {
-  static IMAGE_NAME = 'owentruong/leakage-analysis:latest';
+  static IMAGE_NAME = 'owentruong/leakage-analysis:2.0';
   static CONTAINER_DIR_PATH = '/input';
   static PYTHON_FILE_PATH = path.posix.join(
     this.CONTAINER_DIR_PATH,
